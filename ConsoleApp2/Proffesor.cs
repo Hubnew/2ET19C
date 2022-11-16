25203f816ef2d53207215b786b0f700969ede4e8
5-HJ 2E <101053887+Hubnew@users.noreply.github.com>
2022-11-16 12:17:51 +0100

    Add files via upload

---
+++ ConsoleApp2/Proffesor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Proffesor
+    {
+        public int salary;
+        protected int staffNumber;
+        private int yearsOfService;
+        public int numberOfClasses;
+        public Student supervises;
+    }
+}
