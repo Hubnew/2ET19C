3deb50e3858c78962b0ebd8eaab6e995d2fa99c5
5-HJ 2E <101053887+Hubnew@users.noreply.github.com>
2022-11-23 11:54:12 +0100

    Update oop.cs

--- oop.cs
+++ oop.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -56,18 +57,56 @@ namespace ConsoleApp1
     class Librarian
     {
         public string Name;
-        private string ID;
+        public int ID;
         public int Password;
         public int SearchString;
         public library_database library;
         public Library_Managment_System System;
-        private void Verify_librarian()
+        public List<Book> listOfRentedBooks;
+        public void Verify_librarian()
         {
             Console.WriteLine("verify librarian");
         }
         public void Search()
         {
             Console.WriteLine("search");
+        }
+        public void RentBook(int book_id)
+        {
+            library_database dv = new library_database();
+            Book ksiazka = new Book();
+            dv.Search();
+            listOfRentedBooks.Add(ksiazka);
+            Console.WriteLine("co?");
+        }
+    }
+    class User
+    {
+        public string name;
+        public int id;
+        public void verify()
+        {
+            Console.WriteLine("nie");
+        }
+        public void CheckAccount()
+        {
+            Console.WriteLine("idk");
+        }
+        public void get_book_info()
+        {
+            Console.WriteLine("https://www.youtube.com/watch?v=dHsnzxg51NM");
+        }
+    }
+    class account
+    {
+        public string no_borrowed_books;
+        public string no_reserved_books;
+        public string no_returned_books;
+        public string no_lost_books;
+        public int fine_amount;
+        public void Calculate_fine()
+        {
+            Console.WriteLine("za drogo jak na polskę");
         }
     }
     class library_database
@@ -98,11 +137,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
             Book book = new Book();
             book.Title = "tytul";
             book.Author = "autor";
             book.Book_request();
             book.Renw_info();
+
+            Librarian libek = new Librarian();
+            libek.Name = "Andrzej";
+            libek.ID = 116;
+            libek.Verify_librarian();
+            List<Book> lista = new List<Book>();
+            lista.Add(book1);
+            libek.listofRentedBooks = lista;6
 
             book.librarian = new Librarian();
             book.librarian.Name = "imie";
